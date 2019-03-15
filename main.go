1aec8baf23221e83e5d718ee844492613157b33a
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2019-03-15 16:02:00 +0900

    テンプレートの種類を表示する機能を追加

--- main.go
+++ main.go
@@ -12,26 +12,43 @@ import (
 const url = "https://gitignore.io/api/%s"
 
 var (
+	list = flag.Bool("ls", false, "view available template list")
 	lang = flag.String("lang", "", "choose language")
 )
 
 func main() {
 	flag.Parse()
+	showList()
 	if *lang == "" {
 		log.Fatal("error")
 	}
-	resp, err := http.Get(fmt.Sprintf(url, *lang))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
+	bytes, err := getGitignoreIo(*lang)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := writeToGitignoreFile(&bytes); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func showList() {
+	if *list {
+		bytes, err := getGitignoreIo("list?format=lines")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(fmt.Sprintf("templetes:\n%s", bytes))
+		os.Exit(0)
+	}
+}
+
+func getGitignoreIo(path string) ([]byte, error) {
+	resp, err := http.Get(fmt.Sprintf(url, path))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
 
 func writeToGitignoreFile(bytes *[]byte) error {
